f519cbc39a6a71c83334944fa72c33f8c432c5b4
ThatchanaMoorthy <thatch1614@gmail.com>
2023-10-18 16:01:10 +0530

    modified the file

--- wordguess.py
+++ wordguess.py
@@ -2,11 +2,11 @@ import random
 # library that we use in order to choose
 # on random words from a list of words
 
-name = input("What is your name? ")
+name = input("What is your name?: ")
 
 # Here the user is asked to enter the name first
 
-print("Good Luck ! ", name)
+print("Good Luck buddyyy...... :) ")
 
 words = ['Butterfly', 'Mobile', 'peoples', 'System',
 		'Program', 'function', 'walk', 'tea',
@@ -22,7 +22,7 @@ print("Guess the characters")
 guesses = ''
 
 # any number of turns can be used here
-turns = 12
+turns = 10
 
 
 while turns > 0:
@@ -78,6 +78,6 @@ while turns > 0:
 		print("You have", + turns, 'more guesses')
 
 		if turns == 0:
-			print("You Loose")
+			print("You Loose the game")
 
     
